40f1608d74517fd632eabdb695e259fe38a7e3b0
sis-but-sig <sis.but.sig@gmail.com>
2022-01-04 09:47:04 -0600

    Finding shortest route

--- src/main/java/com/sisbutsig/tddbyexample/shortestroute/NetworkOfRoutes.java
+++ src/main/java/com/sisbutsig/tddbyexample/shortestroute/NetworkOfRoutes.java
@@ -1,11 +1,17 @@
 package com.sisbutsig.tddbyexample.shortestroute;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Stream.concat;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class NetworkOfRoutes {
   private Map<String, Collection<Leg>> legsByFrom = new HashMap<>();
@@ -30,5 +36,56 @@ public class NetworkOfRoutes {
                      .map(Entry::getValue)
                      .map(Collection::size)
                      .reduce(0, Integer::sum);
+  }
+
+  public Collection<Leg> findShortest(String from, String to) {
+    return findShortestRoute(from, to).legs;
+  }
+
+  private Route findShortestRoute(String from, String to) {
+    return legsByFrom.getOrDefault(from, emptyList())
+                     .stream()
+                     .map(leg -> findShortest(leg, to))
+                     .reduce(Route.DEAD_END, Route::shortest);
+  }
+
+  private Route findShortest(Leg leg, String to) {
+    return leg.getTo()
+              .equals(to) ? Route.of(leg)
+                  : Route.of(leg, findShortestRoute(leg.getTo(), to));
+  }
+
+  public static class Route {
+    private static final Route DEAD_END = new Route(emptyList());
+
+    private Collection<Leg> legs;
+    private double totalValue;
+
+    private Route(Collection<Leg> legs) {
+      this.legs = legs;
+      totalValue = legs.stream()
+                       .map(Leg::getValue)
+                       .reduce(0d, Double::sum);
+    }
+
+    private static Route of(Leg leg, Route subroute) {
+      return subroute.isDeadEnd() ? DEAD_END
+          : new Route(
+              concat(Stream.of(leg), subroute.legs.stream()).collect(toList()));
+    }
+
+    private static Route of(Leg leg) {
+      return new Route(singletonList(leg));
+    }
+
+    private static Route shortest(Route route1, Route route2) {
+      return route1.isDeadEnd() ? route2
+          : (route2.isDeadEnd() ? route1
+              : (route1.totalValue < route2.totalValue ? route1 : route2));
+    }
+
+    private boolean isDeadEnd() {
+      return legs.isEmpty();
+    }
   }
 }
--- src/test/java/com/sisbutsig/tddbyexample/shortestroute/GivenANetworkOfRoutes.java
+++ src/test/java/com/sisbutsig/tddbyexample/shortestroute/GivenANetworkOfRoutes.java
@@ -1,5 +1,7 @@
 package com.sisbutsig.tddbyexample.shortestroute;
 
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -43,5 +45,32 @@ public class GivenANetworkOfRoutes {
       assertThat(e.getMessage(),
           is("Redundant leg Leg [from=A, to=B, value=2.0]"));
     }
+  }
+
+  @Test
+  public void findShortestShouldWorkAsExpected() {
+    Leg legAB = Leg.of("A", "B", 1.0);
+    Leg legBC = Leg.of("B", "C", 2.0);
+    Leg legCD = Leg.of("C", "D", 1.0);
+    Leg legAC = Leg.of("A", "C", 2.0);
+    NetworkOfRoutes routes = new NetworkOfRoutes().add(legAB)
+                                                  .add(legBC)
+                                                  .add(legCD)
+                                                  .add(legAC);
+
+    // A to B: [A --1--> B]
+    assertThat(routes.findShortest("A", "B"), contains(legAB));
+    // B to C: [B --2--> C]
+    assertThat(routes.findShortest("B", "C"), contains(legBC));
+    // C to D: [C --1--> D]
+    assertThat(routes.findShortest("C", "D"), contains(legCD));
+    // B to D: [B --2--> C, C --1--> D]
+    assertThat(routes.findShortest("B", "D"), contains(legBC, legCD));
+    // A to C: [A --2--> C]
+    assertThat(routes.findShortest("A", "C"), contains(legAC));
+    // A to D: [A --2--> C, C --1--> D]
+    assertThat(routes.findShortest("A", "D"), contains(legAC, legCD));
+    // A to E: []
+    assertThat(routes.findShortest("A", "E"), empty());
   }
 }
